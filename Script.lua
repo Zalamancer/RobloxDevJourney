6466a64d66c64d38fff7c70f504a4eb09b375cbc
Zalamancer <78333574+Zalamancer@users.noreply.github.com>
2022-07-02 19:01:44 -0500

    Add files via upload

---
+++ Script.lua
@@ -0,0 +1,57 @@
+local DataStoreService = game:GetService("DataStoreService")
+local myDataStore = DataStoreService:GetDataStore("myDataStore")
+
+game.Players.PlayerAdded:Connect(function(player)
+	
+	local leaderstats = Instance.new("Folder")
+	leaderstats.Name = "leaderstats"
+	leaderstats.Parent = player
+	
+	local Cash = Instance.new("IntValue")
+	Cash.Name = "Cash"
+	Cash.Parent = leaderstats
+	
+	local Wins = Instance.new("IntValue")
+	Wins.Name = "Wins"
+	Wins.Parent = leaderstats
+	
+	local playerUserId = "Player_"..player.UserId	
+	
+	-- Load Data
+	
+	local data
+	local success, errormessage = pcall(function()
+		data = myDataStore:GetAsync(playerUserId)
+	end)
+	
+	
+	if success then
+		Cash.Value = data.Cash
+		Wins.Value = data.Wins
+		-- Set our data equal to the currect Cash
+	end
+	
+	
+end)
+
+
+game.Players.PlayerRemoving:Connect(function(player)
+	local playerUserId = "Player_"..player.UserId
+	
+	local data = {
+		Cash = player.leaderstats.Cash.Value;
+		Wins = player.leaderstats.Wins.Value;
+	}
+	
+	local success, errormessage  = pcall(function()
+		myDataStore:SetAsync(playerUserId, data)
+	end)
+	
+	if success then
+		print("Data successfully saved!")
+	else
+		print("There was an error!")
+		warn(errormessage)
+	end
+	
+end)
